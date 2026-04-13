6d1eb1f4ec2b8d2c31a210a5dee0063df79f8bca
abbn7 <dior53634@gmail.com>
2026-04-13 22:39:17 +0000

    Add C:/Users/abdor/Downloads/store NEW/next.config.ts

---
+++ C:/Users/abdor/Downloads/store NEW/next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avdritpwzelxjblwcgwh.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
